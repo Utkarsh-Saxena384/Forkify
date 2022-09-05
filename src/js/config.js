edf8cd159cf23edf6be5cdf518805b09d46d31b6
Utkarsh-Saxena384 <85153929+Utkarsh-Saxena384@users.noreply.github.com>
2022-09-05 16:42:13 +0530

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '35c10d17-3cae-44f4-8dee-6ccd64d108a1';
+export const MODAL_CLOSE_SEC = 2.5;
